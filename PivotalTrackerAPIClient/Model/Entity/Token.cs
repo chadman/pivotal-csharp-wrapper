74c6a5644598dfc1eacce7c7413ada74226c4bae
Chad Meyer <chadmeyer@me.com>
2012-02-19 14:27:45 -0600

    Stubbing out the architecture of the wrapper to see if it all makes sense.

---
+++ PivotalTrackerAPIClient/Model/Entity/Token.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PivotalTrackerAPIClient.Model.Entity {
+
+    /// <summary>
+    /// When a successful attempt to authenticate to Pivotal Tracker is complete, a Token entity is returned
+    /// </summary>
+    public class Token {
+
+        #region Properties
+
+        /// <summary>
+        /// The unique identifier used to pass additional requests to the Pivotal Tracker API
+        /// </summary>
+        public string Guid { get; set; }
+
+        /// <summary>
+        /// The ID of the user that is stored in the database on Pivotal Tracker
+        /// </summary>
+        public int ID { get; set; }
+        #endregion Properties
+    }
+}
